ad258f1161b16aaadadda0886b773a97c1b5cfc6
JuanRamon245 <juanramondeleonmartin@gmail.com>
2025-05-23 23:58:36 +0200

    Primer commit del proyecto del portafolio

---
+++ backend/src/main/java/com/portafolio/backend/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.portafolio.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // permite todas las rutas
+                        .allowedOrigins("http://localhost:4200") // permite Angular
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
